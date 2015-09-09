6cfca326bfa8d7ad9906229c0eb8a0629de1f44b
Kevin Miles <Miles@k.home>
2015-09-08 21:21:43 -0700

    fix

--- index.js
+++ index.js
@@ -1,32 +1,19 @@
-var assert = require('assert'),
-    test = require('selenium-webdriver/testing'),
+var test = require('selenium-webdriver/testing'),
     webdriver = require('selenium-webdriver'),
-    chrome = require('selenium-webdriver/chrome'),
-    firefox = require('selenium-webdriver/firefox'),
-    driver;
-
+    By = webdriver.By,
+    until = webdriver.until;
 test.describe('Google Search', function() {
-  this.timeout(100000);
-
-  test.before(function () {
-    var driver = new webdriver.Builder()
-        .forBrowser('firefox')
-        .build();
+  var driver;
+  test.before(function() {
+    driver = new webdriver.Builder().forBrowser('firefox').build();
   });
-
-  test.after(function () {
+  test.it('should append query to title', function() {
+    driver.get('http://www.google.com');
+    driver.findElement(By.name('q')).sendKeys('webdriver');
+    driver.findElement(By.name('btnG')).click();
+    driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+  });
+  test.after(function() {
     driver.quit();
-  });
-
-  test.it('should work', function(done) {
-    driver.get('http://google.com');
-
-    var searchBox = driver.findElement(webdriver.By.name('q'));
-    searchBox.sendKeys('webdriver');
-    searchBox.getAttribute('value').then(function(value) {
-      assert.equal(value, 'webdriver');
-    });
-
-    done();
   });
 });
